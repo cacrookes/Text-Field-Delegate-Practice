068b69e5981729768eb18b0fe5acbd96645bea87
Christopher Crookes <cacrookes@gmail.com>
2020-07-12 23:13:44 -0700

    Updating comments.

--- Text Field Delegate Practice/ViewController.swift
+++ Text Field Delegate Practice/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - Outlets
     @IBOutlet weak var zipcodeTextField: UITextField!
     @IBOutlet weak var currencyTextField: UITextField!
     @IBOutlet weak var lockableTextField: UITextField!
     @IBOutlet weak var lockTextFieldSwitch: UISwitch!
     
-    
+    // MARK: - Global variables
     let zipcodeTextFieldDelegate = ZipcodeTextFieldDelegate()
     let currencyTextFieldDelegate = CurrencyTextFieldDelegate()
     
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // assign delegates to text fields
         self.zipcodeTextField.delegate = zipcodeTextFieldDelegate
         self.currencyTextField.delegate = currencyTextFieldDelegate
         self.lockableTextField.delegate = self
     }
 
+    // MARK: - UITextField delegate methods
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         return lockTextFieldSwitch.isOn
     }
